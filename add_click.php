c4a69fcb8fe96ee945b66bd0c6d62f9b5d97bbbf
Gabriel Hutchison <ghutchison2600@flpoly.org>
2016-11-05 15:44:23 -0400

    Added error reporting to add_click.php

--- add_click.php
+++ add_click.php
@@ -3,8 +3,11 @@
 try {
     $num = (int) file_get_contents("clicks.txt");
     $num++;
-    file_put_contents("clicks.txt", $num + "");
-    echo $num + "";
+    $success = file_put_contents("clicks.txt", $num + "");
+    if ($success) {
+        echo $num + "";
+    } else {
+        echo "failure";
 } catch (Exception $e) {
     echo "failure";
 }
