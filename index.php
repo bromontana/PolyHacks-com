ff2ccaf256d8aa9e3f14cbb99b110c9b540a8a2e
Frank <frankcalas0193@flpoly.org>
2016-11-23 23:54:06 -0500

    color changes and more reworking

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang=en>
     <head>
+        <meta name="google-site-verification" content="ZMou7wAyGT1u4_EMj8DC00FJhUCdFnN5NL61841EmTA" />
         <meta charset=utf-8>
         <title>PolyHacks - Florida Polytechnic University's Hackathon</title>
 
@@ -33,23 +34,26 @@
     </head>
     <body>
         <nav id=navbar>
-            <a href=#main class=current>
+            <!--<a href=#main class=current>
                 <span>Home<div class=bar></div></span>
+            </a>-->
+            <a href=#main>
+                <span>When?<div class=bar></div></span>
             </a>
             <a href=#qa>
-                <span>Q&amp;A<div class=bar></div></span>
-            </a>
-            <a href=#whatnow>
-                <span>What Now?<div class=bar></div></span>
-            </a>
-            <a href=#countdown>
-                <span>Countdown<div class=bar></div></span>
-            </a>
-            <a href=#sponsors>
-                <span>Sponsors<div class=bar></div></span>
+                <span>What?<div class=bar></div></span>
             </a>
             <a href=#map>
                 <span>Where?<div class=bar></div></span>
+            </a>
+            <a href=#whatnow>
+                <span>How?<div class=bar></div></span>
+            </a>
+            <!--<a href=#countdown>
+                <span>Countdown<div class=bar></div></span>
+            </a>-->
+            <a href=#sponsors>
+                <span>Sponsors<div class=bar></div></span>
             </a>
             <!--<a href=#contact>
                 <span>Contact<div class=bar></div></span>
@@ -59,6 +63,7 @@
                      alt="Click to show links"
                      width=20 height=20>
             </a>
+
         </nav>
         <nav class=hidden><a href="#">d</a></nav>
 
@@ -76,10 +81,13 @@
               <br><br>
             <br>
             <div id=buttons>
+
                 <button id=btnRegister class=showTooltip
-                        tooltip="Sign up by January 3rd to attend!">
+                        tooltip="We will let you know when registration opens up!"
+                        href="https://www.google.com" target=_blank>
                     Register
                 </button>
+
                 <button id=btnVolunteer class=showTooltip
                         tooltip="Help us run the hackathon!">
                     Volunteer
@@ -91,27 +99,58 @@
             </div>
         </section>
 
+
+
         <section id=qa_s>
             <a id=qa class=navLink></a>
             <h1>Q&amp;A</h1>
             <div class=faq>
                 <a href=# class=question>What is a hackathon?</a>
                 <div class=answer>A hackathon is a 24 hour event where people get
-                                  to build a project. </div>
+                                  together and build a project (or hack). The hack is not limited
+                                  in scope, however we ask you to not to start yout hack
+                                  before the hackathon</div>
+            </div>
+            <div class=faq float=right>
+                <a href=# class=question>Can I go?</a>
+                <div class=answer>In order to go, you have to be a student,
+                                  and over the age of 18. </div>
             </div>
             <div class=faq>
-                <a href=# class=question>Questiony Question?</a>
-                <div class=answer>Answery Answer ans-answer</div>
+                <a href=# class=question>Is it free?</a>
+                <div class=answer>Yes! Polyhacks is completely FREE to attend! Food is included in the low, low, price of free</div>
             </div>
             <div class=faq>
-                <a href=# class=question>Questiony Question?</a>
-                <div class=answer>Answery Answer ans-answer</div>
+                <a href=# class=question>What should I bring?</a>
+                <div class=answer>We recommend that you bring a couple things:
+                  <ul>
+                    <li>Your laptop</li>
+                    <li>All your chargers</li>
+                    <li>A pillow and blanket (if you want to sleep)</li>
+                    <li>If you want to hack some hardware, bring everything you can!</li>
+                    <li>Lastly bring your awesome ideas and passion!</li>
+                  </ul>
+                </div>
             </div>
             <div class=faq>
-                <a href=# class=question>Questiony Question?</a>
-                <div class=answer>Answery Answer ans-answer</div>
+                <a href=# class=question>Are there teams?</a>
+                <div class=answer>Absolutely! We strongly enourage being a part of a team.
+                                  Teams can be anywhere between 1 and 4 people and you can
+                                  either come with a team or find one when you get here</div>
             </div>
         </section>
+
+        <section id=map_s>
+            <a id=map class=navLink></a>
+            <div id=mapCover>
+                <div id=mapDarkness>
+                    <div id=mapEnableLabel>Click to view map</div>
+                </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3517.8586799816926!2d-81.85351688463547!3d28.15079001247047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88dd69f623d73aa3%3A0xc795777acc89484!2s4700+Research+Way%2C+Polk+City%2C+FL+33868!5e0!3m2!1sen!2sus!4v1446167223548"
+                        id=imTheMap frameborder="0" allowfullscreen="" class=preventScroll></iframe>
+            </div>
+        </section>
+
 
         <section id=whatnow_s>
             <a id=whatnow class=navLink></a>
@@ -204,30 +243,25 @@
             </div>
         </section>
 
-        <section id=map_s>
-            <a id=map class=navLink></a>
-            <div id=mapCover>
-                <div id=mapDarkness>
-                    <div id=mapEnableLabel>Click to view map</div>
-                </div>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3517.8586799816926!2d-81.85351688463547!3d28.15079001247047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88dd69f623d73aa3%3A0xc795777acc89484!2s4700+Research+Way%2C+Polk+City%2C+FL+33868!5e0!3m2!1sen!2sus!4v1446167223548"
-                        id=imTheMap frameborder="0" allowfullscreen="" class=preventScroll></iframe>
-            </div>
-        </section>
+
 
         <section id=contact_s>
             <a id=contact class=navLink></a>
+            <p>Contact us</p>
             <a class="social fa fa-twitter" id=theTwitterLink
                target=_blank href="https://twitter.com/PolyHacks">
             </a>
             <a class="social fa fa-envelope" id=theEmailLink
                target=_blank href="mailto:hackspoly@gmail.com">
             </a>
+            <a class="social fa fa-facebook" id=theFacebookLink
+               target=_blank href="https://www.facebook.com/hackspoly">
+            </a>
         </section>
 
         <footer id=copyright>
             <a href="https://github.com/PrivacyPolicy/PolyHacks-com"
-               target=_blank>&copy; Gabriel Hutchison</a>
+               target=_blank>&copy; Gabriel Hutchison</a><a> &amp; Frank Calas</a>
         </footer>
 
         <!-- dynamic objects -->
